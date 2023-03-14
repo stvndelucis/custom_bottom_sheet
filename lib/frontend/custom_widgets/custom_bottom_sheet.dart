e49b3b46e5c2f1b3b7e19376454ad8eb01c34131
Steeven Delucis <steevenaime.pro@gmail.com>
2023-03-14 11:47:46 +0100

    Animation de l'opacity

--- lib/frontend/custom_widgets/custom_bottom_sheet.dart
+++ lib/frontend/custom_widgets/custom_bottom_sheet.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 class CustomBottomSheet extends StatefulWidget {
   final Widget child;
   final double minHeight, maxHeight, initialHeight;
+  final ValueChanged<double> onHeightChanged;
 
   const CustomBottomSheet({
     super.key,
@@ -12,6 +13,7 @@ class CustomBottomSheet extends StatefulWidget {
     this.minHeight = 0.3,
     this.maxHeight = 1.0,
     this.initialHeight = 0.5,
+    required this.onHeightChanged,
   });
 
   @override
@@ -21,10 +23,13 @@ class CustomBottomSheet extends StatefulWidget {
 class _CustomBottomSheetState extends State<CustomBottomSheet> {
   late final double _screenHeight;
   double _dragPercentage = 0.0;
+  double? _lastDragPosition, _newDragPosition;
 
   @override
   void initState() {
     super.initState();
+
+    // _dragPercentage = widget.initialHeight;
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _screenHeight = MediaQuery.of(context).size.height;
@@ -35,53 +40,39 @@ class _CustomBottomSheetState extends State<CustomBottomSheet> {
   }
 
   void _onVerticalDragStart(DragStartDetails details) {
-    // _startPosition = details.globalPosition.dy;
-    // log('${details.globalPosition.dy}', name: 'VERTICAL DRAG START | drag position dy');
+    _newDragPosition = details.globalPosition.dy;
   }
 
   void _onVerticalDragUpdate(DragUpdateDetails details) {
     final newPosition = details.globalPosition.dy;
-    // _dragPercentage = (100 - ((newPosition / _screenHeight) * 100)).clamp(widget.minHeight, widget.maxHeight);
-    // _dragPercentage = ((newPosition / _screenHeight) * 100).clamp(widget.minHeight * 100, widget.maxHeight * 100) / 100;
-    _dragPercentage =
-        (((_screenHeight - newPosition) / _screenHeight) * 100).clamp(widget.minHeight * 100, widget.maxHeight * 100) /
-            100;
-    log('$_dragPercentage', name: 'DRAG PERCENTAGE');
+
+    _lastDragPosition = newPosition;
 
     setState(() {
-      // _height = _screenHeight * _dragPercentage;
+      _dragPercentage = (((_screenHeight - newPosition) / _screenHeight) * 100)
+              .clamp(widget.minHeight * 100, widget.maxHeight * 100) /
+          100;
+      log('$_dragPercentage', name: 'DRAG PERCENTAGE');
     });
 
-    // final delta = newPosition - _startPosition;
-    // log('$delta', name: 'VERTICAL DRAG UPDATE | delta');
-
-    // final newHeight = _height - delta;
-    // setState(() {
-    //   _height = newHeight.clamp(widget.minHeight, widget.maxHeight);
-    //   _startPosition = newPosition;
-    // });
-  }
-
-  void _onVerticalDragEnd(DragEndDetails details) {
-    // Optional: Add animation to snap to the closest height value
+    widget.onHeightChanged(_dragPercentage);
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      // onVerticalDragStart: _onVerticalDragStart,
+      onVerticalDragStart: _onVerticalDragStart,
       onVerticalDragUpdate: _onVerticalDragUpdate,
-      onTap: () {},
-      // onVerticalDragEnd: _onVerticalDragEnd,
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 150),
-        curve: Curves.linear,
-        decoration: BoxDecoration(
+      onTap: () {}, // Pour annuler le fait de cliquer sans drag
+      child: ClipRRect(
+        borderRadius: const BorderRadius.vertical(top: Radius.circular(15.0)),
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 150),
+          curve: Curves.linear,
           color: Theme.of(context).primaryColor,
-          borderRadius: const BorderRadius.vertical(top: Radius.circular(15.0)),
+          height: MediaQuery.of(context).size.height * _dragPercentage,
+          child: widget.child,
         ),
-        height: MediaQuery.of(context).size.height * _dragPercentage,
-        child: widget.child,
       ),
     );
   }
--- lib/frontend/pages/home_page.dart
+++ lib/frontend/pages/home_page.dart
@@ -11,14 +11,107 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var _opacity = 1.0;
+  double? _lastSheetPercentage;
+
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      bottomSheet: CustomBottomSheet(
-        minHeight: 0.3,
-        maxHeight: 0.9,
-        child: Center(child: Text('Hello')),
-      ),
-    );
+  Widget build(BuildContext context) => Scaffold(
+        bottomSheet: CustomBottomSheet(
+          minHeight: 0.4,
+          maxHeight: 0.9,
+          onHeightChanged: (sheetPercentage) {
+            if (_lastSheetPercentage != null) {
+              if (sheetPercentage > _lastSheetPercentage!) {
+                // Le bottom sheet monte
+                _updateOpacity('decrease');
+              } else if (sheetPercentage < _lastSheetPercentage!) {
+                // Le bottom sheet descend
+                _updateOpacity('increase');
+              }
+            } else {
+              _lastSheetPercentage = sheetPercentage;
+
+              _updateOpacity('decrease');
+            }
+          },
+          child: Column(
+            children: [
+              //
+              _buildHeader(),
+
+              const SizedBox(height: 10.0),
+
+              AnimatedOpacity(
+                opacity: _opacity,
+                duration: const Duration(milliseconds: 800),
+                child: Container(
+                  height: 120,
+                  decoration: const BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+
+  void _updateOpacity(String action) {
+    if (action == 'decrease') {
+      final newOpacity = _opacity - 0.1;
+      if( newOpacity < 0) {
+        _opacity = 0.0;
+      } else{
+        _opacity = newOpacity;
+      }
+    } else if (action == 'increase') {
+      final newOpacity = _opacity + 0.1;
+      if (newOpacity > 1.0) {
+        _opacity = 1.0;
+      }else {
+        _opacity = newOpacity;
+      }
+    }
+    setState(() {});
   }
+
+  Widget _buildHeader() => SizedBox(
+        height: 100.0,
+        child: Row(
+          children: [
+            //
+            Flexible(
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.orange,
+                  borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                ),
+              ),
+            ),
+
+            const SizedBox(width: 5.0),
+
+            Flexible(
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.orange,
+                  borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                ),
+              ),
+            ),
+
+            const SizedBox(width: 5.0),
+
+            Flexible(
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.orange,
+                  borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
 }
